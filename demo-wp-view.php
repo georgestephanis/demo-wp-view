5887528ee2d807f52340d05e477d20c99708e3ce
George Stephanis <george@stephanis.info>
2014-12-02 14:03:59 -0500

    Initial plugin file creation.

---
+++ demo-wp-view.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Demo WP View
+Plugin URI:  http://github.com/georgestephanis/demo-wp-view
+Description: A demo for using Editor Views in WordPress 3.9+
+Author:      George Stephanis
+Version:     0.1
+Author URI:  http://stephanis.info/
+*/
