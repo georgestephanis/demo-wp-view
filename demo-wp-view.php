c0862ebad3c8164b27691515b2b276043ce3074d
George Stephanis <george@stephanis.info>
2014-12-02 14:13:23 -0500

    Add the shortcode definition.

--- demo-wp-view.php
+++ demo-wp-view.php
@@ -8,3 +8,13 @@ Author:      George Stephanis
 Version:     0.1
 Author URI:  http://stephanis.info/
 */
+
+function demo_wp_view_shortcode( $args ){
+	$defaults = array(
+		'content' => __( 'Please specify some content.' ),
+	);
+	$args = shortcode_atts( $defaults, $args, 'demo_wp_view' );
+
+	return sprintf( '<div class="demo_wp_view">%s</div>', esc_html( wp_kses( $args['content'], array() ) ) );
+}
+add_shortcode( 'demo_wp_view', 'demo_wp_view_shortcode' );
