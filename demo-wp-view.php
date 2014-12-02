762e7aeebe460f244f80251477272729567f0827
George Stephanis <george@stephanis.info>
2014-12-02 15:03:37 -0500

    View Implementation.
    I think this is right.
    
    Simplified and loosely based off of
    https://github.com/dtbaker/wordpress-mce-view-and-shortcode-editor/

--- demo-wp-view.php
+++ demo-wp-view.php
@@ -18,3 +18,83 @@ function demo_wp_view_shortcode( $args ){
 	return sprintf( '<div class="demo_wp_view" style="background: #f00;">%s</div>', esc_html( wp_kses( $args['content'], array() ) ) );
 }
 add_shortcode( 'demo_wp_view', 'demo_wp_view_shortcode' );
+
+function demo_wp_view_js_template() {
+	if ( ! isset( get_current_screen()->id ) || get_current_screen()->base != 'post' ) {
+		return;
+	}
+	?>
+	<script type="text/html" id="tmpl-demo_wp_view_shortcode">
+		<div class="demo_wp_view" style="background: #f00;">{{ data.content }}</div>
+	</script>
+	<?php
+}
+add_action( 'admin_print_footer_scripts', 'demo_wp_view_js_template' );
+
+/**
+ * For convenience and readability, this is printed out in the
+ * footer, but ideally should be enqueued seperately.
+ */
+function demp_wp_view_footer_scripts() {
+	if ( ! isset( get_current_screen()->id ) || get_current_screen()->base != 'post' ) {
+		return;
+	}
+	?>
+	<script>
+		/* global tinyMCE, console */
+		(function( $, wp ){
+			wp.mce = wp.mce || {};
+			wp.mce.demo_wp_view_renderer = {
+				shortcode_data : {},
+				View : {
+					template   : wp.template( 'demo_wp_view_shortcode' ),
+					initialize : function( options ) {
+						this.shortcode = options.shortcode;
+						// Do any needed tweaking here
+						wp.mce.demo_wp_view_renderer.shortcode_data = this.shortcode;
+					},
+					getHtml : function() {
+						var options = this.shortcode.attrs.named;
+						// Do any needed tweaking here
+						return this.template( options );
+					}
+				},
+				edit: function( node ) {
+					var values = this.shortcode_data.attrs.named;
+					// Do any needed tweaking here
+					wp.mce.demo_wp_view_renderer.popupwindow( tinyMCE.activeEditor, values );
+				},
+				popupwindow: function( editor, values, onsubmit_callback ){
+					if ( typeof onsubmit_callback != 'function' ) {
+						onsubmit_callback = function( e ) {
+							var s = '[demo_wp_view',
+								i;
+							for ( i in e.data ) {
+								if ( e.data.hasOwnProperty( i ) ) {
+									s += ' ' + i + '="' + e.data[ i ] + '"';
+								}
+							}
+							s += ']';
+							editor.insertContent( s );
+						};
+					}
+					editor.windowManager.open( {
+						title : 'Demo WP View', // This should be internationalized via wp_localize_script
+						body  : [
+							{
+								type  : 'textbox',
+								name  : 'content',
+								label : 'Content', // This should be internationalized via wp_localize_script
+								value : values.content
+							}
+						],
+						onsubmit : onsubmit_callback
+					} );
+				}
+			};
+			wp.mce.views.register( 'demo_wp_view', wp.mce.demo_wp_view_renderer );
+		}( jQuery, wp ));
+	</script>
+	<?php
+}
+add_action( 'admin_print_footer_scripts', 'demp_wp_view_footer_scripts' );
